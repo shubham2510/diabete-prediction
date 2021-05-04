f7ab972c0be9c090cc4b026523fb99ed38b8d936
shubzz wani <shubham281098@gmail.com>
2021-05-04 15:31:54 +0530

    Add files via upload

---
+++ diabetes (1).sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 03, 2021 at 07:39 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `diabetes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `name` varchar(255) NOT NULL,
+  `mobile` int(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `messageData` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`name`, `mobile`, `email`, `messageData`, `id`) VALUES
+('shubham', 78979877, 's@gmail.com', 'fddgds', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `predict`
+--
+
+CREATE TABLE `predict` (
+  `id` int(10) NOT NULL,
+  `uid` varchar(10) NOT NULL,
+  `glucose` varchar(10) NOT NULL,
+  `insulin` varchar(10) NOT NULL,
+  `bmi` varchar(10) NOT NULL,
+  `age` varchar(10) NOT NULL,
+  `predict` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `predict`
+--
+
+INSERT INTO `predict` (`id`, `uid`, `glucose`, `insulin`, `bmi`, `age`, `predict`) VALUES
+(24, '2', '140', '0', '33.4', '45', '1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mobile` int(15) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `blood` varchar(25) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `date` varchar(25) NOT NULL,
+  `doctor` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `name`, `mobile`, `email`, `blood`, `address`, `date`, `doctor`, `password`) VALUES
+(2, 'shubham', 2147483647, 'shubham281098@gmail.com', 'A+', 'Jalgaon', '2021-05-01', 'Dr.Girish Narkhede', '12345678');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `predict`
+--
+ALTER TABLE `predict`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `predict`
+--
+ALTER TABLE `predict`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
